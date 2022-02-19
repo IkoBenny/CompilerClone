c8f5af3a97d736fc7d5052eafba42ac20a88caaa
Benny Iko <benny.iko.it@gmail.com>
2022-02-19 13:19:11 -0500

    Changes to be committed: 	new file:   p4test.java     On branch master 	Changes: 	N/A 	Issues: 	N/A

---
+++ p4test.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+
+public class p4test<T>
+{
+    // HashMap mapTtoInteger = new HashMap<T, Integer>();
+ public static void main(String a[]){
+        // HashMap<String, String> hm = new HashMap<String, String>();
+        // //add key-value pair to hashmap
+        // hm.put("first", "FIRST INSERTED");
+        // hm.put("second", "SECOND INSERTED");
+        // hm.put("third","THIRD INSERTED");
+        // System.out.println(hm);
+        // //getting value for the given key from hashmap
+        // System.out.println("Value of second: "+hm.get("second"));
+        // System.out.println("Is HashMap empty? "+hm.isEmpty());
+        // hm.remove("third");
+        // System.out.println(hm);
+        // System.out.println("Size of the HashMap: "+hm.size());
+        DGraph test = new DGraph();
+        System.out.println(test.hm.isEmpty());
+        test.hm.put("Benny", 0);
+    }}
+
